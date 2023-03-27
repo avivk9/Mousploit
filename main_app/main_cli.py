492784f7f1183dd83525d88a5babd1a821f65b7e
Itamar-Azmoni <itamarazmoni123@gmail.com>
2023-03-27 21:49:52 +0300

    Build CLI

--- main_app/main_cli.py
+++ main_app/main_cli.py
@@ -1,22 +1,19 @@
 import colorama
 from colorama import Fore, Style
+import sys
+import os
 
 colorama.init(autoreset=True)
 
 def print_help():
-    print("helping")
-    pass
+    print("""
+        Choose an option:
+        '1' - Perform a keystroke injection attack against a specified target.
+        '2' - Scan for nearby vulnerable devices.
+        '3' - Exit.
+          """)
 
 def print_logo():
-    # Print the headline with colorized text
-    # print(f'{Fore.GREEN}\
-    # _      ____  _     ____  ____  _     ____  _  _____    \n\
-    # / \__/|/  _ \/ \ /\/ ___\/  __\/ \   /  _ \/ \/__ __\   \n\
-    # | |\/||| / \|| | |||    \|  \/|| |   | / \|| |  / \     \n\
-    # | |  ||| \_/|| \_/|\___ ||  __/| |_/\| \_/|| |  | |     \n\
-    # \_/  \|\____/\____/\____/\_/   \____/\____/\_/  \_/     \n\
-    # ')
-
     g = Fore.LIGHTGREEN_EX # green
     w = Style.RESET_ALL # white
     # printing a raw string, in which the backslash character is treated as a literal character
@@ -42,29 +39,37 @@ def print_logo():
     print("Mousploit - Proof of Concept")
     #print("Final Project in Cyber Security - College of Management")
     print("Team: Ron Greenberg, Aviv Keinan, Itamar Azmoni, Yonatan Birman\n")
-    print("for help use 'help', to exit use 'exit'")
-
 
 def main():
     print_logo()
     while True:
+        print_help() # print options
         request = input("> ")
         
-        if request.__contains__('exit'):
-            print("bye!")
-            break
-        
-        if request.__contains__('help'):
-            print_help()
-            continue
-        
-        # example - TODO: CHANGE
-        if request.__contains__('scan'):
-            print('scanning...')
-            continue
+        cmd = "python mousploit.py "
+        if request == "1": # in case of attack
+            address = input("Enter address")
+            if address != "":
+                cmd += f"--address {address} "
+            opt = input("""
+                        Enter '1' for string.
+                        Enter '2' for script.""")
+            if opt == '1': # inject string
+                string = input("Enter string")
+                cmd += f"--string \"{string}\""
+            elif opt == '2': # inject duckyscript
+                path = input("Enter path of duckyscript file")
+                cmd += f"--script-file \"{path}\""
 
-        # anything else:
-        print('what do u mean?')
+        elif request == "2": # in case of scan
+            duration = input("Enter duration")
+            cmd += f"scan --duration {duration}"
+
+        elif request == "3": # in case of exit
+            print("bye!")
+            sys.exit()
+
+        os.system(cmd)
 
 if __name__ == '__main__':
     main()
